bb4eb3391c9994cac3705468ea3f77881f911e00
zhengyuanhong <1713639570@qq.com>
2019-01-03 17:32:19 +0800

    web

---
+++ app/Http/Controllers/testController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class testController extends Controller{
+
+	public function index(){
+
+	
+		return 'hellozhengCOntroller';
+
+	}
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+o<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/test', 'testController@index');
+
+Route::get('/a',function(){
+
+return 'hellozheng';
+});
